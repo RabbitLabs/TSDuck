53806095e12cb3e8c282ccdb9532ea93fb5302c3
Gilles <gilles@rabbitlabs.com>
2023-04-11 13:02:40 +0200

    fix syntax in script

--- run.sh
+++ run.sh
@@ -1,7 +1,7 @@
 #!/usr/bin/env bash
 
 # get files from S3
-if  [[ -n $S3_ENDPOINT && -n $S3_BUCKET && -n $S3_ACCESS_KEY && -n $S3_ACCESS_TOKEN && -n $S3_SECRET_KEY ]]
+if  [[ -n $S3_ENDPOINT && -n $S3_BUCKET && -n $S3_ACCESS_KEY && -n $S3_ACCESS_TOKEN && -n $S3_SECRET_KEY ]] then
     echo "Getting files from S3 ..."
     s3cmd --host=$S3_ENDPOINT --access_key=$S3_ACCESS_KEY --secret_key=$S3_SECRET_KEY --access_token=$S3_ACCESS_TOKEN --no-progress --recursive sync s3://$S3_BUCKET/ /data/
 fi
