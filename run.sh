152be0be79121a9b794984e959d2af65b446a25c
Gilles <gilles@rabbitlabs.com>
2023-04-11 20:09:01 +0200

    fix way of passing S3 credentials

--- run.sh
+++ run.sh
@@ -4,7 +4,7 @@
 if  [[ -n $S3_ENDPOINT && -n $S3_BUCKET ]] then
     echo "Getting files from S3 ..."
     if [[ -n $S3_SECRET_KEY && -n S3_ACCESS_KEY ]] then
-      s3cmd --host=$S3_ENDPOINT --access_key=$S3_ACCESS_KEY --secret_key=$S3_SECRET_KEY --no-progress --recursive sync s3://$S3_BUCKET/ /data/
+      s3cmd --host=$S3_ENDPOINT --host-bucket=$S3_BUCKET --access_key=$S3_ACCESS_KEY --secret_key=$S3_SECRET_KEY --no-progress --recursive sync s3:// /data/
     else if [[ -n $S3_ACCESS_TOKEN ]] then
         s3cmd --host=$S3_ENDPOINT --no-progress --recursive --access_token=$S3_ACCESS_TOKEN sync s3://$S3_BUCKET/ /data/
       fi
